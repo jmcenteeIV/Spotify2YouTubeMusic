66951dc453ec4e65ba47678601cee0d75be196b8
Jansen McEntee <jmcenteeiv@gmail.com>
2022-02-06 15:09:18 -0700

    add some skeleton to project

---
+++ spotify2youtube/__init__.py
@@ -0,0 +1,16 @@
+"""
+Convert playlists between streaming providers
+"""
+
+test = "Hello World"
+print(f"{test}")
+if True:
+    test = "hello world"
+print(f"{test}")
+for x in range(1):
+    test = "helloworld!"
+print(f"{test}")
+test2 = ""
+for x in range(1):
+    test2 = "helloworld!"
+print(f"{test2}")

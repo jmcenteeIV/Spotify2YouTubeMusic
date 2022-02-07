a0d8788f598a3b63b28295d30e528582c559c31f
Jansen McEntee <jmcenteeiv@gmail.com>
2022-02-06 18:54:42 -0700

    initial implementation for spotify class

--- spotify2youtube/__init__.py
+++ spotify2youtube/__init__.py
@@ -1,16 +1,7 @@
+import core
+
 """
 Convert playlists between streaming providers
 """
 
-test = "Hello World"
-print(f"{test}")
-if True:
-    test = "hello world"
-print(f"{test}")
-for x in range(1):
-    test = "helloworld!"
-print(f"{test}")
-test2 = ""
-for x in range(1):
-    test2 = "helloworld!"
-print(f"{test2}")
+core.main()
--- spotify2youtube/core.py
+++ spotify2youtube/core.py
@@ -1,3 +1,13 @@
-class demon_stugg:
-    print("hello")
-    print("world")
+from lib import spotify, youtube_music
+
+"""
+Entry file for spotify2youtube
+"""
+
+
+def main():
+    """
+    Entry method for spotify2youtube
+    """
+    spotify_instance = spotify.Spotify()
+    spotify_instance.get_playlists()
